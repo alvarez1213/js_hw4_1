2b07ed386616608e49a94013ddb2ab3f0943b8ba
Dmitry Melnikov <d.melnikov.alex@gmail.com>
2023-11-12 16:39:02 +0300

    Initial commit

---
+++ task.js
@@ -0,0 +1,37 @@
+document.addEventListener("click", (e) => {
+    // получаем таргет
+    const target = e.target    
+
+    // проверяем является ли элемент подсказкой
+    if (Array.from(target.classList).includes('has-tooltip')) {        
+        // определяем координаты таргета
+        let targetRect = target.getBoundingClientRect()
+        
+        // создаем элемент, координаты со смещением от таргета
+        let tip = document.createElement('div')
+        tip.className = 'tooltip tooltip_active'
+        tip.style.left = (targetRect.x - tip.offsetHeight + 20) + 'px'
+        tip.style.top = (targetRect.y - tip.offsetHeight + 20) + 'px'        
+        tip.textContent = target.title
+
+        // добавляем элемент на страницу
+        document.body.append(tip)        
+    }
+
+    // отключаем переход по ссылке
+    e.preventDefault()
+})
+
+document.addEventListener("mouseout", () => {
+    const tip = [...document.querySelectorAll('.tooltip')]
+    
+    for (const t of tip) {
+        if (t != null) {        
+            // TODO: можно удалить класс активности 
+            // или удалить элемент целиком, вопрос: что лучше?
+            // t.classList.remove('tooltip_active')
+                        
+            t.remove()
+        }   
+    }  
+})
